2d87329d511de46309ee6faeb418f56cf5815294
jgradone <joe@gradone.com>
2025-09-12 12:52:30 -0400

    update __init__.py in analysis

---
+++ src/slocum_ad2cp/analysis/__init__.py
@@ -0,0 +1,3 @@
+from .analysis import get_erddap_dataset, erddapy, grid_glider_data, profile_mld, gsw_rho, dist_from_lat_lon
+
+__all__ = ["get_erddap_dataset","erddapy", "grid_glider_data", "profile_mld", "gsw_rho", "dist_from_lat_lon"]
