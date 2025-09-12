4e86bfc8025db2076a02055c81f4f5a6d5045eb6
jgradone <jgradone@marine.rutgers.edu>
2025-09-12 12:07:41 -0400

    Edited src
    Edited src to allow for pip install and import slocum_ad2cp

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Processing and analysis of TWR Slocum Glider-Nortek AD2CP data',
-    author='Joe Gradone',
-    license='MIT',
+    name="Slocum-AD2CP",  # distribution name (what pip sees)
+    version="0.1.0",
+    description="Processing and analysis of TWR Slocum Glider-Nortek AD2CP data",
+    author="Joe Gradone",
+    license="MIT",
+    packages=find_packages(where="src"),   # look inside src/
+    package_dir={"": "src"},               # root is src/
+    install_requires=[
+        # add dependencies here, e.g. "numpy", "xarray"
+    ],
+    python_requires=">=3.8",
 )
