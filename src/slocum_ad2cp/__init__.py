30cda47390a7cf254695507cc6d3dfdb4d860d58
jgradone <joe@gradone.com>
2025-09-12 13:17:47 -0400

    h

--- src/slocum_ad2cp/__init__.py
+++ src/slocum_ad2cp/__init__.py
@@ -1,3 +1,24 @@
 from .analysis import get_erddap_dataset, erddapy, grid_glider_data, profile_mld, gsw_rho, dist_from_lat_lon
 
-__all__ = ["get_erddap_dataset","erddapy", "grid_glider_data", "profile_mld", "gsw_rho", "dist_from_lat_lon"]
+from .dataset import check_max_beam_range, check_max_beam_range_bins, check_mean_beam_range, check_mean_beam_range_bins, beam2enu, beam_true_depth, binmap_adcp, cell_vert, correct_sound_speed, qaqc_pre_coord_transform, qaqc_post_coord_transform, inversion, mag_var_correction, shear_method
+
+__all__ = ["get_erddap_dataset",
+           "erddapy", 
+           "grid_glider_data", 
+           "profile_mld", 
+           "gsw_rho", 
+           "dist_from_lat_lon", 
+           "check_max_beam_range", 
+           "check_max_beam_range_bins", 
+           "check_mean_beam_range", 
+           "check_mean_beam_range_bins",
+           "beam2enu", 
+           "beam_true_depth", 
+           "binmap_adcp", 
+           "cell_vert", 
+           "correct_sound_speed", 
+           "qaqc_pre_coord_transform",
+           "qaqc_post_coord_transform",
+           "inversion",
+           "mag_var_correction", 
+           "shear_method"]
