2ad689c5cff42be061dc4dd6f8baf44321fc5f0e
Joe Gradone <43152605+JGradone@users.noreply.github.com>
2025-09-15 08:33:41 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Slocum-AD2CP",  # distribution name (what pip sees)
+    name="slocum-ad2cp",  # 🔑 PyPI package name (must be lowercase, hyphens allowed)
     version="0.1.0",
     description="Processing and analysis of TWR Slocum Glider-Nortek AD2CP data",
     author="Joe Gradone",
@@ -15,6 +15,16 @@ setup(
         "netCDF4",
         "scipy",
         "erddapy",
-        "gsw"],
-    python_requires=">=3.8",
+        "gsw",
+    ],
+    python_requires=">=3.9",  # keep consistent with pyproject.toml
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/JGradone/Slocum-AD2CP",
+    project_urls={
+        "Bug Tracker": "https://github.com/JGradone/Slocum-AD2CP/issues",
+    },
 )
